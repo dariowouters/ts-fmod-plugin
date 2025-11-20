c75ee4a87f1b295b5f7d98a581295443fc5692d1
Dario Wouters <dariowouters@gmail.com>
2025-11-20 16:21:18 +0100

    Support for 1.57

--- ts-fmod-plugin/common.h
+++ ts-fmod-plugin/common.h
@@ -2,8 +2,8 @@
 
 namespace common
 {
-    inline const char* plugin_version = "1.56";
-    constexpr uint32_t supported_game_version = 56;
+    inline const char* plugin_version = "1.57";
+    constexpr uint32_t supported_game_version = 57;
 
     inline FMOD_GUID get_guid(const std::string& s_guid)
     {
